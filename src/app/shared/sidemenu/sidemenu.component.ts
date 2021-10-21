0058a88612fa9d7f7b0bec135e406aa39f53fb6c
Pancho <panchon.ues@gmail.com>
2021-10-21 00:14:43 -0700

    sidemenu

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  texto: string;
+  ruta: string;
+}
 
 @Component({
   selector: 'app-sidemenu',
@@ -6,11 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent {
 
-  constructor() { }
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'basicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'dinamicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'switches',
+      ruta: './template/switches'
+    }
+  ];
 
-  ngOnInit(): void {
-  }
-
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'basicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'dinamicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'switches',
+      ruta: './reactive/switches'
+    }
+  ];
+  
 }
