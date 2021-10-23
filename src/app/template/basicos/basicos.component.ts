fbddbbe2e03b5fab6bc8660f0fe46bd56fddaea0
Pancho <panchon.ues@gmail.com>
2021-10-22 23:10:56 -0700

    viewchild & numbervalid

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,13 +9,23 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar(form: NgForm){
-    console.log(form);
+  nombreValido(): boolean {
+    return this.miFormulario?.controls.producto?.invalid && this.miFormulario.controls.producto?.touched;
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls.precio?.invalid && this.miFormulario.controls.precio?.touched;
+  }
+
+  guardar(){
+    console.log(this.miFormulario);
   }
 
 }
