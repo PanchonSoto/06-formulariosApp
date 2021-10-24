8d8bab1a95d4898d2739efa2dee71802532dbede
Pancho <panchon.ues@gmail.com>
2021-10-23 20:53:31 -0700

    custom directives, clear form

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -11,6 +11,12 @@ export class BasicosComponent implements OnInit {
 
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
+  initForm = {
+    producto: 'RTX 6969',
+    precio: 10,
+    stock: 5
+  }
+
   constructor() { }
 
   ngOnInit(): void {
@@ -26,6 +32,11 @@ export class BasicosComponent implements OnInit {
 
   guardar(){
     console.log(this.miFormulario);
+
+    this.miFormulario.resetForm({
+      precio: 0,
+      stock: 0
+    });
   }
 
 }
