e9d790b7e2517684a0fe431017b62b3e4a4c6b2f
Pancho <panchon.ues@gmail.com>
2021-10-24 11:10:46 -0700

    agregar elementos,borrar y switches

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +16,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  nuevoJuego: string = '';
+  persona: Persona = {
+    nombre: 'Pancho',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear' },
+      { id: 2, nombre: 'The walking dead' }
+    ]
   }
 
+  agregarJuego(){
+    const nuevoFav: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFav});
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index,1);
+  }
+ 
+  guardar(){
+    console.log('Formulario posteado');
+  }
 }
