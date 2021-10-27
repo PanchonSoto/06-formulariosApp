66a4b24c5c157b943983724f8f8f008a7d77a70f
Pancho <panchon.ues@gmail.com>
2021-10-27 15:09:56 -0700

    forms groups and validators

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  /* miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('RTX 4080ti'),
+    precio: new FormControl(50000),
+    stock: new FormControl(20)
+  }); */
 
-  ngOnInit(): void {
-  }
+  miFormulario: FormGroup = this.fbuilder.group({
+    nombre: ['RTX 4080ti', [Validators.required, Validators.minLength(3)] ],
+    precio: [0, [Validators.min(5000), Validators.required] ],
+    stock: [0, [Validators.min(0), Validators.required] ]
+  });
+
+  constructor(private fbuilder: FormBuilder) { }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
@@ -15,7 +16,9 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveFormsModule,
+    ReactiveRoutingModule,
+    
   ]
 })
 export class ReactiveModule { }
