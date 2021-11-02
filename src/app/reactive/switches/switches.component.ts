dc322c458b37e80dfb071ca8e6759dfa7eef8a2a
Pancho <panchon.ues@gmail.com>
2021-11-02 15:37:54 -0700

    reactiveForms: Switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miForm: FormGroup = this.fbuilder.group({
+    genero: ['M',Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [true, Validators.requiredTrue]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  };
+
+  constructor(private fbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miForm.reset({
+      ...this.persona, 
+      condiciones: false
+    });
+
+    this.miForm.valueChanges.subscribe(form=>{
+      delete form.condiciones;
+      this.persona = form;  
+    });
+  }
+
+  guardar(){
+    const formValue = {...this.miForm.value };
+    delete formValue.condiciones;
+    console.log(formValue);
   }
 
 }
