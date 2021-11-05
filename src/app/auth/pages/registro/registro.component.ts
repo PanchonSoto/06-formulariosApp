e91ba8d0c4c7fd71d7943ae0aa92618f61a41b88
Pancho <panchon.ues@gmail.com>
2021-11-04 18:27:05 -0700

    validacion de: nombre,email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  //TODO: Temporal
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  miFormulario: FormGroup = this.fbuilder.group({
+    nombre: ['',[Validators.required, Validators.pattern(this.nombreApellidoPattern)] ],
+    email: ['',[Validators.required, Validators.pattern(this.emailPattern)] ]
+  });
+
+  constructor(private fbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.miFormulario.reset({
+      nombre: 'Pancho Soto',
+      email: 'test1@gmail.com'
+    });
+  }
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.get(campo)?.invalid
+            && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitForm() {
+    console.log(this.miFormulario.value);
+    
+    this.miFormulario.markAllAsTouched();
   }
 
 }
